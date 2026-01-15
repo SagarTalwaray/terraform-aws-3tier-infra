d8e7ce41156281b663f60547f476a617f76b3bb0
Sagar Talwaray <75831480+SagarTalwaray@users.noreply.github.com>
2026-01-15 14:27:04 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,12 @@
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "public_subnets" {
+  value = module.vpc.public_subnet_ids
+}
+
+output "alb_dns_name" {
+  value = module.alb_asg.alb_dns_name
+}
 
