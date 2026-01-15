131947871f6e4c5072c71d30e8d76df3922fc517
Sagar Talwaray <75831480+SagarTalwaray@users.noreply.github.com>
2026-01-15 14:26:43 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,18 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "eu-west-2"
+}
+
+variable "project_name" {
+  description = "Project name prefix"
+  type        = string
+  default     = "terraform-aws-3tier"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
 
