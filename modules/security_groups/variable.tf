a10acff6c82685cf3433144074059b9d37051160
Sagar Talwaray <75831480+SagarTalwaray@users.noreply.github.com>
2026-01-15 14:46:27 +0000

    Update variable.tf

--- modules/security_groups/variable.tf
+++ modules/security_groups/variable.tf
@@ -1,1 +1,20 @@
+variable "vpc_id" {
+  type = string
+}
+
+variable "project_name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "alb_cidr_ingress" {
+  type = list(string)
+}
+
+variable "ssh_cidr_ingress" {
+  type = list(string)
+}
 
